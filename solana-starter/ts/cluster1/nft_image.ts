58566dd15cfd0e92239d7adb5471338004b95f0f
haiiru <jj4068851@gmail.com>
2025-01-16 13:14:39 +0000

    initial commit

---
+++ solana-starter/ts/cluster1/nft_image.ts
@@ -0,0 +1,31 @@
+import wallet from "../Turbin3-wallet1.json"
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
+import { createGenericFile, createSignerFromKeypair, signerIdentity } from "@metaplex-foundation/umi"
+import { irysUploader } from "@metaplex-foundation/umi-uploader-irys"
+import { readFile } from "fs/promises"
+
+// Create a devnet connection
+const umi = createUmi('https://api.devnet.solana.com');
+
+let keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
+const signer = createSignerFromKeypair(umi, keypair);
+
+umi.use(irysUploader());
+umi.use(signerIdentity(signer));
+
+(async () => {
+    try {
+        //1. Load image
+        const imageBuffer = await readFile()
+        //2. Convert image to generic file.
+        //3. Upload image
+
+        // const image = ???
+
+        // const [myUri] = ??? 
+        // console.log("Your image URI: ", myUri);
+    }
+    catch(error) {
+        console.log("Oops.. Something went wrong", error);
+    }
+})();
