fda0f0db9f5de5875950bcc66307609db4247888
haiiru <jj4068851@gmail.com>
2025-01-17 01:17:19 +0000

    feat: add mint nft

--- solana-starter/ts/cluster1/nft_mint.ts
+++ solana-starter/ts/cluster1/nft_mint.ts
@@ -2,8 +2,10 @@ import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
 import { createSignerFromKeypair, signerIdentity, generateSigner, percentAmount } from "@metaplex-foundation/umi"
 import { createNft, mplTokenMetadata } from "@metaplex-foundation/mpl-token-metadata";
 
-import wallet from "../wba-wallet.json"
+import wallet from "../Turbin3-wallet1.json"
 import base58 from "bs58";
+import path from "path";
+import { readFile } from "fs/promises";
 
 const RPC_ENDPOINT = "https://api.devnet.solana.com";
 const umi = createUmi(RPC_ENDPOINT);
@@ -16,11 +18,19 @@ umi.use(mplTokenMetadata())
 const mint = generateSigner(umi);
 
 (async () => {
-    // let tx = ???
-    // let result = await tx.sendAndConfirm(umi);
-    // const signature = base58.encode(result.signature);
+    const nftName = 'generug-metadata';
+    const nftMetadataPath = path.resolve(__dirname,`../nft-link/${nftName}.txt`)
+    const nftLink = await readFile(nftMetadataPath, 'utf-8');
+    let tx = await createNft(umi,{
+        mint: mint,
+        name: 'Haiiru Code NFT',
+        uri: nftLink,
+        sellerFeeBasisPoints: percentAmount(1)
+    })
+    let result = await tx.sendAndConfirm(umi);
+    const signature = base58.encode(result.signature);
     
-    // console.log(`Succesfully Minted! Check out your TX here:\nhttps://explorer.solana.com/tx/${signature}?cluster=devnet`)
+    console.log(`Succesfully Minted! Check out your TX here:\nhttps://explorer.solana.com/tx/${signature}?cluster=devnet`)
 
     console.log("Mint Address: ", mint.publicKey);
 })();
